0c623df22eaaaaa284af6369e6b70400223cf715
codeTester7 <testerqualityengineer7@gmail.com>
2024-07-15 16:53:19 +0530

    changes added

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -1,12 +1,19 @@
 const {test, expect} = require('@playwright/test');
 
-test('Item Purchase on Amazon', async ({page}) => {
+test.beforeEach('Run before each test', async ({page}) => {
     await page.goto('https://www.amazon.in/');
+});
+
+test.afterEach('Run after each test', async ({page}) => {
+    await page.waitForTimeout(5000);
+});
+
+test('Item Purchase on Amazon', async ({page}) => {
     await page.click('#twotabsearchtextbox');
     await page.fill('#twotabsearchtextbox', 'iphone 15');
     await page.click('#nav-search-submit-button');
     await page.waitForTimeout(3000);
     await page.getByRole('link', {name: 'Apple iPhone 15 (128 GB) - Black'}).nth(1).click();
     // await page.waitForTimeout(5000);
-    await page.click("//input[@id='add-to-cart-button']");
+    // await page.getByRole('input', {value : 'Add to Cart'}).click();
 })
