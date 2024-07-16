b12a64f4b183419206b0e8e0855c3486d4feee29
codeTester7 <testerqualityengineer7@gmail.com>
2024-07-16 10:41:25 +0530

    config edited

--- playwright.config.js
+++ playwright.config.js
@@ -40,10 +40,10 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     }
 ,
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',
